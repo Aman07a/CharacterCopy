eaebef632738f61aa8438bcf0bfbd847a8a85caa
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-27 13:49:19 +0200

    Worked Example: The Character Copy Kata

--- character-copy.ts
+++ character-copy.ts
@@ -1,15 +1,17 @@
 export interface Source {
   readChar(): string;
 }
-
 export interface Destination {
   writeChar(c: string): void;
 }
-
 export class Copier {
   constructor(private source: Source, private destination: Destination) {}
 
   copy() {
-    this.destination.writeChar(this.source.readChar());
+    let char = this.source.readChar();
+    while (char !== "\n") {
+      this.destination.writeChar(char);
+      char = this.source.readChar();
+    }
   }
 }
